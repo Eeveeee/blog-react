5d85a742eacf7ab36a8e6c7bc78fdf8e7be19f42
Eeveeee <fx8300@gmail.com>
2022-02-04 00:14:02 +0300

    heroku commit

--- src/assets/icons/global/GlobalSvgSelector.jsx
+++ src/assets/icons/global/GlobalSvgSelector.jsx
@@ -5,21 +5,18 @@ function GlobalSvgSelector({ id }) {
         <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M7.5 17.5H4.16667C3.72464 17.5 3.30072 17.3244 2.98816 17.0118C2.67559 16.6993 2.5 16.2754 2.5 15.8333V4.16667C2.5 3.72464 2.67559 3.30072 2.98816 2.98816C3.30072 2.67559 3.72464 2.5 4.16667 2.5H7.5"
-            stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
           <path
             d="M11.1665 6.00016L6.99984 10.1668L11.1665 14.3335"
-            stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
           <path
             d="M7.5 10L17.5 10"
-            stroke="white"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
