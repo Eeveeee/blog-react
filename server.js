0ac9e66b761a4a9901e5b9b4a513132024603ef6
Eeveeee <fx8300@gmail.com>
2022-02-03 23:49:54 +0300

    heroku commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
-const __dirname = path.dirname('/');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
